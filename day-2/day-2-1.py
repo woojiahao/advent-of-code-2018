ca946596227e78ca55cc7f5d763a9b314a3c980a
woojiahao <woojiahao1234@gmail.com>
2018-12-02 15:22:36 +0800

    Completed day 2 puzzle 1

---
+++ day-2/day-2-1.py
@@ -0,0 +1,18 @@
+from itertools import groupby
+
+def has_repeat(word: str, count: int) -> bool:
+  return any(i == count for i in [len(''.join(grp)) for _, grp in groupby(list(sorted(word.lower())))])
+
+def count_repeats() -> tuple:
+  _twos = 0
+  _threes = 0
+
+  with open('data.txt', 'r') as file:
+    for line in [line.replace('\n', '') for line in file.readlines()]:
+      if has_repeat(line, 2): _twos += 1
+      if has_repeat(line, 3): _threes += 1
+
+  return _twos, _threes
+
+twos, threes = count_repeats()
+print(f'Checksum: {twos * threes}')
